3c119c4d9fd74fa9e5479c13acc2718f9484a264
Veronika Revyakina <veronikaw777@gmail.com>
2021-08-17 18:41:04 +0300

    initial configuration up tp Lessen 6, used constructor injection and constactor-arg, IoC

---
+++ src/main/java/com/revyakina/springcourse/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package com.revyakina.springcourse;
+
+public class ClassicalMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Hungarian Rapsody";
+    }
+}
---
+++ src/main/java/com/revyakina/springcourse/Music.java
@@ -0,0 +1,5 @@
+package com.revyakina.springcourse;
+
+public interface Music {
+    String getSong();
+}
